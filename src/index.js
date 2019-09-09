1a7a50baacf06daf92c3cbe16ce81f7869743425
Ahmet Can Güven <ahmetcanguven44@gmail.com>
2019-09-09 11:01:37 +0300

    Token support

--- src/index.js
+++ src/index.js
@@ -1,72 +1,89 @@
-const fetch = require('node-fetch');
-const https = require("https");
-const http = require('http');
-
-const agentConfig = {
-  rejectUnauthorized: false
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+  return (mod && mod.__esModule) ? mod : {"default": mod};
 };
+Object.defineProperty(exports, "__esModule", {value: true});
+const rxjs_1 = require("rxjs");
+require("rxjs/add/observable/from");
+const request_1 = __importDefault(require("request"));
+const operators_1 = require("rxjs/operators");
+const JSONStream_1 = __importDefault(require("JSONStream"));
+const event_stream_1 = __importDefault(require("event-stream"));
+var KUBE_RESPONSE_TYPE;
+(function (KUBE_RESPONSE_TYPE) {
+  KUBE_RESPONSE_TYPE["ADDED"] = "ADDED";
+  KUBE_RESPONSE_TYPE["MODIFIED"] = "MODIFIED";
+})(KUBE_RESPONSE_TYPE || (KUBE_RESPONSE_TYPE = {}));
 
-const httpsAgent = new https.Agent(agentConfig);
-const httpAgent = new http.Agent(agentConfig);
+class ConfigUpdater {
+  constructor(config) {
+    this.configMapValues = {};
+    this.configuration = config;
+    this.start();
+  }
 
-const defaultConfiguration = {
-  overwriteExisting: true,
-  updateUrl: null,
-  frequency: 1000 * 60 * 5,
-  exclude: []
-};
+  start() {
+    this.k8sUpdate = rxjs_1.Observable.create((subscriber) => this.getRequestStream(subscriber));
+    this.k8sUpdate
+      .pipe(operators_1.map(this.mapResponse))
+      .pipe(operators_1.filter((update) => update.kind === 'ConfigMap'))
+      .subscribe(this.updateHandler.bind(this));
+  }
 
-const mapEnvironment = (configResponse, config, cb) => {
-  Object.keys(configResponse.data).forEach(key => {
-    if (config.exclude.indexOf(key) === -1) {
-      if (process.env[key]) {
-        if (config.overwriteExisting) {
-          process.env[key] = configResponse.data[key];
-        }
+  getRequestStream(observer) {
+
+    const headers = {};
+
+    if (this.configuration.auth) {
+      if (this.configuration.auth.token) {
+        headers['authorization'] = 'Bearer ' + this.configuration.auth.token;
       } else {
-        process.env[key] = configResponse.data[key];
+        headers['authorization'] = 'Basic ' + Buffer.from(this.configuration.auth.username + ':' + this.configuration.auth.password).toString('base64');
       }
     }
-  });
-  cb && cb();
-};
 
-const update = (config, cb) => {
-  const headers = {};
-
-  if (config.auth) {
-    if(config.auth.token) {
-      headers['authorization'] = 'Bearer ' + config.auth.token;
-    } else {
-      headers['authorization'] = 'Basic ' + Buffer.from(config.auth.username + ':' + config.auth.password).toString('base64');
-    }
-  }
-
-  fetch(config.updateUrl, {agent: (config.updateUrl.indexOf('https') > -1 ? httpsAgent : httpAgent), headers})
-    .then(res => res.json())
-    .then(data => mapEnvironment(data, config, cb))
-    .catch(err => {
-      console.error('Config update error:', err)
+    const req = request_1.default({
+      url: `${this.configuration.url}/api/v1/namespaces/${this.configuration.namespace}/configmaps?watch=1`,
+      headers,
+      strictSSL: false
     });
-};
-
-module.exports = config => {
-  const configuration = {
-    ...defaultConfiguration,
-    ...config
-  };
-
-  if (!configuration.updateUrl) {
-    throw new Error('Missing configuration {updateUrl}, example: http://kubeip:4444/api/v1/namespaces/default/configmaps/acg')
+    req.on('complete', () => {
+      console.warn('JSON stream ended, refreshing connection');
+      this.getRequestStream(observer);
+    });
+    req.on('error', (err) => {
+      console.error('An error occurred on JSON stream', err);
+      this.getRequestStream(observer);
+    });
+    req.on('abort', () => {
+      console.error('An error occurred on JSON stream');
+      this.getRequestStream(observer);
+    });
+    req
+      .pipe(JSONStream_1.default.parse())
+      .pipe(event_stream_1.default.mapSync((data) => observer.next(data)));
+    return req;
   }
 
-  const instance = setInterval(() => update(configuration), configuration.frequency);
+  mapResponse(response) {
+    return {
+      type: response.type,
+      data: response.object.data,
+      kind: response.object.kind,
+      name: response.object.metadata.name
+    };
+  }
 
-  return {
-    configuration,
-    update: (cb) => update(configuration, cb),
-    stop: () => {
-      clearInterval(instance)
-    }
-  };
-};
+  updateHandler(kubeResponse) {
+    this.configMapValues[kubeResponse.name] = kubeResponse.data || Object.assign(this.configMapValues[kubeResponse.name] || {}, kubeResponse.data);
+    rxjs_1.Observable
+      .from(this.configuration.configmaps)
+      .reduce((configResult, key) => Object.assign(configResult, this.configMapValues[key] || {}), {})
+      .subscribe((configObject) => {
+        console.info('Environment configuration is updated.');
+        Object.assign(process.env, configObject);
+      });
+  }
+}
+
+module.exports = ConfigUpdater;
